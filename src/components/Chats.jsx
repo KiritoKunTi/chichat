f3ad73d3a450f22b670a33b696b5865e9f40b1f6
duman <duman200354@mail.ru>
2022-11-25 23:44:09 +0600

    sidebar comp style

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="user__chat">
+        <img
+          src="https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg"
+          alt=""
+        />
+        <div className="user__chat_info">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="user__chat">
+        <img
+          src="https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg"
+          alt=""
+        />
+        <div className="user__chat_info">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="user__chat">
+        <img
+          src="https://static.boredpanda.com/blog/wp-content/uploads/2014/01/animal-children-photography-elena-shumilova-2.jpg"
+          alt=""
+        />
+        <div className="user__chat_info">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="search__form">
+        <input type="text" placeholder="find a user" />
+      </div>
+      <div className="user__chat">
+        <img
+          src="https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg"
+          alt=""
+        />
+        <div className="user__chat_info">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
