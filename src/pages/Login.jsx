e137716c6d79d4585579f5e75a48609f04a7bb35
duman <duman200354@mail.ru>
2022-11-25 21:41:02 +0600

    login style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="form__container">
+      <div className="form_wrapper">
+        <h1 className="logo">ChiChat</h1>
+        <p className="title">Login</p>
+        <form>
+          <input type="email" tabIndex={2} placeholder="email" />
+          <input type="password" tabIndex={3} placeholder="password" />
+          <button>Log In</button>
+        </form>
+        <p>Do not have an account? Register</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
