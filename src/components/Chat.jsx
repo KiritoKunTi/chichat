9200716bf11297447acd428eb56e8f74bb6b47a0
duman <duman200354@mail.ru>
2022-11-26 00:23:45 +0600

    input comp style

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BsCameraVideoFill } from "react-icons/bs";
+import { FaUserPlus } from "react-icons/fa";
+import { MdOutlineMoreHoriz } from "react-icons/md";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__info">
+        <span>Jane</span>
+        <div className="chat__icons">
+          <BsCameraVideoFill className="icon" size={18} />
+          <FaUserPlus className="icon" size={18} />
+          <MdOutlineMoreHoriz className="icon" size={18} />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { RiImageAddLine } from "react-icons/ri";
+import { IoMdAttach } from "react-icons/io";
 
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <IoMdAttach className="icon" size={24} />
+        <input type="file" style={{ display: "none" }} id="image" />
+        <label htmlFor="image">
+          <RiImageAddLine className="icon" size={24} />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
