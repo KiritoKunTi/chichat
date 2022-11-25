23c2a6df17a5094f11c627f88b1449610125bf7f
duman <duman200354@mail.ru>
2022-11-26 00:49:02 +0600

    message comp style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="message__info">
+        <img
+          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="message__content">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
