0862acac914a286f7c0691432550c7d5eba728d9
duman <duman200354@mail.ru>
2022-12-04 09:01:17 +0600

    search func

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDoc,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { UserAuth } from "../contexts/AuthContext";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+
+  const { currentUser } = UserAuth();
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    if (currentUser.uid === user.uid) return;
+
+    // check whether chat exist
+    const combinedID =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedID));
+
+      if (!res.exists()) {
+        //create one
+        await setDoc(doc(db, "chats", combinedID), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedID + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedID + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setUser(null);
+    setUsername("");
+  };
+
   return (
     <div className="search">
       <div className="search__form">
-        <input type="text" placeholder="find a user" />
-      </div>
-      <div className="user__chat">
-        <img
-          src="https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg"
-          alt=""
+        <input
+          type="text"
+          value={username}
+          placeholder="find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <div className="user__chat_info">
-          <span>Jane</span>
-        </div>
       </div>
+      {error && <span>{error}</span>}
+      {user && (
+        <div className="user__chat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="user__chat_info">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
